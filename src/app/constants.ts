bd966d968987cfb042f211110949e7b7e66c6366
Syafiq Shamsuddin <Shyuffix555@gmail.com>
2019-08-12 22:21:56 +0800

    seperate url with constants

---
+++ src/app/constants.ts
@@ -0,0 +1,1 @@
+export const URL: string = "http://localhost/volunteer-sarawak/public/"
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,30 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Profile } from '../models/profile.model';
 import { HttpClient } from '@angular/common/http';
+import { URL } from 'url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private _userIsAuthenticated = false;
-  url : string = "http://localhost/volunteer-sarawak-api/public/"
 
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   authenticated() {
     this._userIsAuthenticated = true;
   }
 
   login(profile: Profile) {
-    return this.http.post(this.url + 'login', profile);
+    return this.http.post(URL + 'login', profile);
   }
 
   createAcc(profile: Profile) {
-    return this.http.post(this.url + 'register', profile);
+    return this.http.post(URL + 'register', profile);
   }
 
   logout() {
