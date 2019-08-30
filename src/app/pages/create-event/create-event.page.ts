5f00198262f7777e1f8382d233d3accc14189bed
MUHAMMAD IRSYAD BIN KAMIL RIADZ <irsyadkamil96@gmail.com>
2019-08-30 22:27:19 +0800

    back button

--- src/app/pages/create-event/create-event.page.ts
+++ src/app/pages/create-event/create-event.page.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEventPage implements OnInit {
 
-  constructor() {
+  constructor(private location: Location) {
   }
 
   ngOnInit() {
   }
 
+
+goBack() {
+    this.location.back();
+  }
 }
